15a564b4a05a6cb6afed4a3223396821c0e64fb7
Wingemo <philip.wingemo@outlook.com>
2021-09-13 13:44:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const model = require("./model");
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '..', 'client')))
@@ -11,6 +12,18 @@ app.use(express.static(path.join(__dirname, '..', 'client')))
 io.on('connection', (socket) => {
   socket.on('message', (msg) => {
       io.emit('message', msg);
+  });
+});
+
+// New 
+io.on('connection', (socket) => {
+  socket.on('create', (data) => {
+      model.create(data);
+      io.emit('create', data);
+  });
+  socket.on('delete', (data) => {
+      model.create(data);
+      io.emit('delete', data);
   });
 });
 

45c5b1636dfe2c86d1b8ac6db0c76683e6417a42
Wingemo <philip.wingemo@outlook.com>
2021-09-13 17:24:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,17 +12,17 @@ app.use(express.static(path.join(__dirname, '..', 'client')))
 io.on('connection', (socket) => {
   io.to(socket.id).emit(model.read());
   socket.on('create', (input) => {
-      io.emit('create', await model.create(input));
+      const data = await model.create(input))
+      io.emit('create', data);
   });
   socket.on('delete', (input) => {
       response = await model.delete(input);
       io.emit('delete', response);
   });
   socket.on('update', (input) => {
-      const doc = await model.findOne(info)
-      doc.slug = input;
-      await doc.save()
-      io.emit('update', response);
+      const data = await model.findOne(info)
+      data.accountbalance = input;
+      io.emit('update', await doc.save());
   });
 });
 

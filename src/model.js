28e9fe5c691a5b6876a2530f407e5dc891595ec2
Wingemo <philip.wingemo@outlook.com>
2021-09-13 17:34:30 +0200

    Update model.js

--- src/model.js
+++ src/model.js
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose';
 
+/**
+ * Initialize constant with an array of strings.
+ * Loop through each item in the array and print
+ * it to the console.
+ */ 
 const { Schema } = mongoose;
 const accountSchema = new Schema({
   accountnumber: {
